5f9fa3ccec15b86ad0282ed5c1cbc3c5b9f02275
IT19006994 - K.H.K.L. De Silva <87925569+IT19006994@users.noreply.github.com>
2022-11-05 19:55:33 +0530

    Add User interface added

--- Frontend/abc_frontend/src/App.js
+++ Frontend/abc_frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import LogIn from './components/LogIn'
 import AddMessageComponent from './components/AddMessageComponent'
 import AddFileComponent from './components/AddFileComponent'
+import AddUser from './components/AddUser'
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
         <Route path="/login" exact component={LogIn} />
         <Route path="/addMessage" exact component={AddMessageComponent} />
         <Route path="/addFile" exact component={AddFileComponent} />
+        <Route path="/addUser" exact component={AddUser} />
       </div>
     </Router>
   )
---
+++ Frontend/abc_frontend/src/components/AddUser.jsx
@@ -0,0 +1,90 @@
+import React, {Component} from 'react';
+import Header from "./Header";
+
+class AddUser extends Component {
+    render() {
+        return (
+            <div>
+                <div>
+                    <br/><br/><br/><br/>
+                    <div className="container">
+                        <Header></Header>
+                        <div className="row">
+                            <form>
+                                <div className="mb-3">
+                                    <label className="form-label"><i className="fa fa-user-o" aria-hidden="true"></i>&nbsp;
+                                        Add User
+                                    </label>
+                                </div>
+                                <div className="mb-3">
+                                    <div className="input-group mb-3">
+                                        <input type="text" name="name" id="name" placeholder="Enter Name" className="form-control"/>
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <div className="input-group mb-3">
+                                        <textarea type="text" name="userName" id="userName" placeholder="Enter Username" className="form-control" />
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <div className="input-group mb-3">
+                                        <div className="input-group mb-3">
+                                            <select className="custom-select" name="roleId" id="roleId" placeholder="Role ID"
+                                                    style={{height:"45px",width:"100%"}}>
+                                                <option selected>Choose Role ID...</option>
+                                                <option value="Research Paper Presentation">Manager</option>
+                                                <option value="Research Paper Presentation">Worker</option>
+                                            </select>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <div className="input-group mb-3">
+                                        <textarea type="text" name="email" id="email" placeholder="Enter Email" className="form-control" />
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <div className="input-group mb-3">
+                                        <textarea type="text" name="password" id="password" placeholder="Enter Password" className="form-control" />
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <div className="input-group mb-3">
+                                        <textarea type="text" name="contact" id="contact" placeholder="Enter Contact No" className="form-control" />
+                                    </div>
+                                </div>
+                                <div className="mb-3">
+                                    <div className="input-group mb-3">
+                                        <textarea type="text" name="occupation" id="occupation" placeholder="Enter Occupation" className="form-control" />
+                                    </div>
+                                </div>
+
+                            </form>
+                        </div>
+                    </div>
+                    <div className="container mb-3">
+                        <div className="row">
+                            {/*Add Notice Button*/}
+                            <button type="button" className="btn btn-primary"><i className="fa fa-floppy-o" aria-hidden="true"></i>&nbsp;
+                                Add User
+                            </button>
+                        </div>
+                        <br/>
+                        <div className="row">
+                            {/*Cancel Button*/}
+                            <button type="button" className="btn btn-danger" data-bs-dismiss="modal">
+                                <i className="fa fa-times" aria-hidden="true"></i>&nbsp;
+                                Cancel
+                            </button>
+                        </div>
+
+
+                    </div>
+                </div>
+
+            </div>
+        );
+    }
+}
+
+export default AddUser;
