a421a03fee4b4865af012d481f408f99c46da782
Janith Senanayake <janith@edhirya.com>
2022-11-17 11:07:02 +0530

    fixes

--- backend-application/src/main/java/com/ssd/backendapplication/service/OTPServiceImpl.java
+++ backend-application/src/main/java/com/ssd/backendapplication/service/OTPServiceImpl.java
@@ -1,5 +1,7 @@
 package com.ssd.backendapplication.service;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.ssd.backendapplication.auth.jwt.UsernameAndPasswordAuthenticationRequest;
 import com.ssd.backendapplication.model.OTP;
 import com.ssd.backendapplication.model.User;
 import com.ssd.backendapplication.repository.OTPRepository;
@@ -7,17 +9,24 @@ import com.ssd.backendapplication.repository.UserAttemptsRepository;
 import com.ssd.backendapplication.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.Optional;
 
 @Slf4j
 @Service
 @AllArgsConstructor
-public class OTPServiceImpl implements OTPService{
+public class OTPServiceImpl implements OTPService {
 
     private final OTPRepository otpRepository;
     private final UserRepository userRepository;
+    private final RestTemplate restTemplate;
 
     private final UserAttemptsRepository userAttemptsRepository;
 
@@ -44,16 +53,31 @@ public class OTPServiceImpl implements OTPService{
     }
 
     @Override
-    public Optional<User> verifyOTP(String id, int otp) {
+    public Object verifyOTP(String id, int otp) {
         Optional<OTP> otpObj = otpRepository.findById(id);
-        if(otpObj.isPresent()){
+        if (otpObj.isPresent()) {
             Optional<User> user = userRepository.findById(otpObj.get().getUserId());
-            if(otpObj.get().getOtp() == otp && user.isPresent()){
+            if (otpObj.get().getOtp() == otp && user.isPresent()) {
                 otpRepository.deleteById(id);
                 userAttemptsRepository.deleteById(user.get().getUserName());
-                return user;
+                return getAccessToken(user.get());
             }
         }
         return null;
+    }
+
+    public Object getAccessToken(User user) {
+
+        String url = "https://localhost:443/api/login";
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setContentType(MediaType.APPLICATION_JSON);
+        UsernameAndPasswordAuthenticationRequest usernameAndPasswordAuthenticationRequest = new UsernameAndPasswordAuthenticationRequest();
+        usernameAndPasswordAuthenticationRequest.setPassword(user.getPassword());
+        usernameAndPasswordAuthenticationRequest.setUsername(user.getUserName());
+
+        HttpEntity<UsernameAndPasswordAuthenticationRequest> requestEntity = new HttpEntity<>(usernameAndPasswordAuthenticationRequest, httpHeaders);
+        ResponseEntity<JsonNode> jsonNode = restTemplate.exchange(url, HttpMethod.POST, requestEntity, JsonNode.class);
+        log.info("token get by otp {}", jsonNode.getBody());
+        return jsonNode.getBody();
     }
 }
